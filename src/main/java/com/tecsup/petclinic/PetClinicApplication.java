8f4a7d6af038321f30cf4e3bbca45ecda63145ed
Valeriano Daniel <daniel.valeriano@tecsup.edu.pe>
2021-10-25 17:01:07 -0500

    First commit

---
+++ src/main/java/com/tecsup/petclinic/PetClinicApplication.java
@@ -0,0 +1,23 @@
+package com.tecsup.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ *
+ */
+@SpringBootApplication
+public class PetClinicApplication {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
+
+//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   mi token 
